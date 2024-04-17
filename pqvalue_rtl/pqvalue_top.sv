67d443ef70db37d86601652d56d6700c5b003c76
LeonFuchs <leon.fuchs@etu.emse.fr>
2024-04-17 17:08:06 +0200

    Change to fit IBEX, WIP

--- pqvalue_rtl/pqvalue_top.sv
+++ pqvalue_rtl/pqvalue_top.sv
@@ -1,51 +1,46 @@
 //pqvalue_top.sv
-//Single-cycle Cooley-Tukey and Gentleman-Sande butterflies
+//Custom ALU top module
+//Adapted for register files with 2 read ports and 1 write port
 
-//sel_butterfly_i :
-//0 : Cooley-Tukey
-//1 : Gentleman-Sande
+//sel_op_i :
+//00 : Unused
+//01 : pq.mod_add
+//10 : pq.mod_sub
+//11 : pq.mod_mul
 
 //sel_red_i :
 //0 : Dilithium
 //1 : Kyber
 
-`timescale 1 ns/ 1 ps
-
 module pqvalue_top (
-	input logic [23:0] a_i, b_i,
-	input logic [22:0] twiddle_i,
-	input logic sel_red_i, sel_butterfly_i,
-	output logic [22:0] a_o, b_o
+	input logic [22:0] a_i, b_i,
+	input logic sel_red_i,
+	input logic [1:0] sel_op_i,
+	output logic [22:0] operand_o;
 	);
-	logic [23:0] add_in_s, sub_in_s;
-	logic [22:0] mul_in_s, mul_out_s, sub_out_s, q_s;
-
-	assign add_in_s = sel_butterfly_i ? b_i : {1'b0,mul_out_s};
-	assign sub_in_s = sel_butterfly_i ? b_i : {1'b0,mul_out_s};
-	assign mul_in_s = sel_butterfly_i ? sub_out_s : b_i[22:0];
-	assign q_s = sel_red_i ? 3329 : 8380417;
+	logic [22:0] add_out_s, sub_out_s, mul_out_s;
 
 	mod_add mod_add (//Modular addition
-		.a_i(a_i),
-		.b_i(add_in_s),
-		.q_i(q_s),
-		.c_o(a_o)	
+		.a_i({1'b0,a_i}),
+		.b_i({1'b0,b_i}),
+		.q_i(sel_red_i ? 3329 : 8380417),
+		.c_o(add_out_s)	
 		);
 
 	mod_sub mod_sub (//Modular subtraction
-		.a_i(a_i),
-		.b_i(sub_in_s),
-		.q_i(q_s),
+		.a_i({1'b0,a_i}),
+		.b_i({1'b0,b_i}),
+		.q_i(sel_red_i ? 3329 : 8380417),
 		.c_o(sub_out_s)	
 	);
 
 	mod_mul mod_mul (//Modular multiplication
-		.a_i(twiddle_i),
-		.b_i(mul_in_s),
+		.a_i({1'b0,a_i}),
+		.b_i({1'b0,b_i}),
 		.select_i(sel_red_i),
 		.c_o(mul_out_s)	
 	);
 
-	assign b_o = sel_butterfly_i ? mul_out_s : sub_out_s;
+	assign operand_o = 0;//WIP
 
 endmodule : butterfly
